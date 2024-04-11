ea1d2bd4f34a9145c203775c37646cb5268ed46a
emaphis <emaphis85@gmail.com>
2024-04-11 09:59:16 -0400

    Updates, create a template (MainX.scala) for new sessions

---
+++ devinside2/scala-playlist/src/main/scala/MainX.scala
@@ -0,0 +1,14 @@
+// Part X - Template for new classes
+
+object MainX {
+
+    def mainM(args: Array[String]): Unit = {
+        println("~" * 50)
+
+        {
+            println("Hello MainX")
+        }
+
+        println("~" * 50)
+    }
+}
