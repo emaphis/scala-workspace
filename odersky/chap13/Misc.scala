18e4f18e099c893ce4ffd93291d204e2dcfcaef4
Ed <emaphis85@gmail.com>
2026-05-02 23:09:33 -0400

    More chapter 12

--- odersky/chap13/Misc.scala
+++ odersky/chap13/Misc.scala
@@ -1,6 +1,8 @@
 // Chapter 13
 // Pattern Matching
 
+package chap13
+
 // 13.1 A simple example
 
 object Misc {
@@ -43,16 +45,118 @@ object Misc {
       case Nil => "the empty list"
       case _ => "Something else"
 
+  // Variable patterns
+
+  // Listing 13.6 · A pattern match with a variable pattern
+  def startsWithZero(expr: Any): String =
+    expr match
+      case 0 => "zero"
+      case somethingElse => s"not zero $somethingElse"
+
+  // Constructor patterns
+
+  // Listing 13.7 · A pattern match with a constructor pattern.
+  def deepMatch(expr: Expr): String =
+    expr match
+      case BinOp("+", e, Num(0)) => "a deep match"
+      case _ => ""
+
+  // Sequence patterns
+
+  // Listing 13.8 · A sequence pattern with a fixed length.
+  def startsWithZero1(xs: List[Int]): String =
+    xs match
+      case List(0, _, _) => "found it"
+      case _ => ""
+
+  // Listing 13.9 · A sequence pattern with an arbitrary length.
+  def startsWithZero2(xs: List[Int]): String =
+    xs match
+      case List(0, _*) => "found it"
+      case _ => ""
+
+  // Tuple patterns
+
+  // Listing 13.10 · A pattern match with a tuple pattern.
+  def tupleDemo(obj: Any): String =
+    obj match
+      case (a, b, c) => s"matched $a$b$c"
+      case _ => ""
+
+  // Typed patterns
+
+  // Listing 13.11 · A pattern match with typed patterns
+  def generalSize(x: Any): Int =
+    x match
+      case s: String   => s.length
+      case m: Map[_,_] => m.size
+      case _   => -1
+
+  // Listing 13.12 · Using isInstanceOf and asInstanceOf (poor style).
+  def isInstance1(expr: Any): Int =
+    if (expr.isInstanceOf[String]) {
+      val s = expr.asInstanceOf[String]
+      s.length
+    }
+    else 0
+
+  // better
+  def isInstance2(expr: Any): Int =
+    expr match {
+      case s: String =>
+        s.length
+      case _ => 0
+    }
+
+  // type erasure
+
+  def isIntIntMap(x: Any): Boolean =
+    x match
+      case m: Map[Int, Int] => true  // cant be checked at runtime
+      case _ => false
+
+  // Arrays are checked at runtime
+  def isStringArray(x: Any): String =
+    x match
+      case a: Array[String] => "yes"
+      case _ => "no"
+
+  // 13.3 Pattern guards
+
+  // Listing 13.14 · A match expression with a pattern guard.
+  def simplifyAdd(e: Expr): Expr =
+    e match
+      case BinOp("+", x, y) if x == y =>
+        BinOp("*", x, Num(2))
+      case _ => e
+  
+  // 13.4 Pattern overlaps
+
+  // Listing 13.15 · Match expression in which case order matters.
+  def simplifyAll(expr: Expr): Expr =
+    expr match
+      case UnOp("-", UnOp("-", e)) =>
+        simplifyAll(e)
+      case BinOp("+", e, Num(0)) =>
+        simplifyAll(e)
+      case BinOp("*", e, Num(1)) =>
+        simplifyAll(e)
+      case UnOp(op, e) =>
+        UnOp(op, simplifyAll(e))
+      case BinOp(op, l, r) =>
+        BinOp(op, simplifyAll(l), simplifyAll(r))
+      case _ => expr
+
 
   def main(args: Array[String]): Unit = {
     val v = Var("x")
-    val op = BinOp("+", Num(1), v)
-    println(op)
+    val op1 = BinOp("+", Num(1), v)
+    println(op1)
 
     val op2 = UnOp("-", UnOp("-", v))
     println(op2)
 
-    println("Simplify op: " + simplifyTop(op))
+    println("Simplify op1: " + simplifyTop(op1))
     println("Simplify op2: " + simplifyTop(op2))
 
     val varExpr = Var("var")
@@ -71,5 +175,36 @@ object Misc {
     println(describe("hello"))
     println(describe(Nil))
     println(describe(List(1, 2, 3)))
+
+    println(startsWithZero(0))
+    println(startsWithZero("Any Thing"))
+
+    println(deepMatch(zeroExpr))
+    println(deepMatch(BinOp("+", varExpr, zeroExpr)))
+
+    println(startsWithZero1(List(0, 1, 2)))
+    println(startsWithZero1(List(2, 3, 4)))
+
+    println(startsWithZero2(List(0)))
+    println(startsWithZero2(List(0, 1, 2)))
+    println(startsWithZero2(List(2, 3, 4)))
+    println(startsWithZero2(List(1)))
+
+    println(tupleDemo(("a ", 3, "-tuple")))
+
+    println(generalSize("abc"))
+    println(generalSize(Map(1 -> 'a', 2 -> 'b')))
+    println(generalSize(math.Pi))
+
+    println(isInstance2("foo"))
+    println(isInstance2(333))
+
+    println(isIntIntMap(Map(1 -> 1)))
+    println(isIntIntMap(Map("abc" -> "abc")))  // oops
+
+    println(isStringArray(Array("abc")))
+    println(isStringArray(Array(1, 2, 3)))
+
+    println(simplifyAdd(BinOp("+", Num(4), Num(4))))
   }
 }
--- odersky/chap13/patternmatching.worksheet.sc
+++ odersky/chap13/patternmatching.worksheet.sc
@@ -79,3 +79,134 @@ describe("hello")
 describe(Nil)
 describe(List(1,2,3))
 
+//Listing 13.6 · A pattern match with a variable pattern.
+def startsWithZero(expr: Any): String =
+  expr match
+    case 0 => "zero"
+    case somethingElse => s"not zero $somethingElse"
+
+startsWithZero(0)
+startsWithZero("Any thing else")
+
+//Variable or constant?
+import math.{E, Pi}
+
+E match
+  case Pi => s"strange math? pi = $Pi"
+  case _ => "Ok"
+
+val pi = math.Pi
+
+E match
+  case pi => s"strange math? pi = $pi"
+ // case _ => "Ok"
+
+// Listing 13.7 · A pattern match with a constructor pattern.
+def deepMatch(expr: Expr): String =
+  expr match
+    case BinOp("+", e, Num(0)) => "a deep match"
+    case _  => ""
+
+deepMatch(zeroExpr)
+deepMatch(BinOp("+", varExpr, zeroExpr))
+
+// Sequence patterns
+
+// Listing 13.8 · A sequence pattern with a fixed length.
+def startsWithZero1(xs: List[Int]): String =
+  xs match
+    case List(0, _, _) => "found it"
+    case _  => ""
+
+startsWithZero1(List(0, 1, 2))
+startsWithZero1(List(2, 3, 4))
+
+// Listing 13.9 · A sequence pattern with an arbitrary length.
+def startsWithZero2(xs: List[Int]): String =
+  xs match
+    case List(0, _*) => "found it"
+    case _  => ""
+
+startsWithZero2(List(0))
+startsWithZero2(List(0, 1, 2))
+startsWithZero2(List(2, 3, 4))
+startsWithZero2(List(1))
+
+// Listing 13.10 · A pattern match with a tuple pattern.
+def tupleDemo(obj: Any): String =
+  obj match
+    case (a, b, c) => s"matched $a$b$c"
+    case _  => ""
+
+tupleDemo(("a ", 3, "-tuple"))
+
+// Listing 13.11 · A pattern match with typed patterns
+def generalSize(x: Any): Int =
+  x match
+    case s: String   => s.length
+    case m: Map[_,_] => m.size
+    case _   => -1
+
+generalSize("abc")
+generalSize(Map( 1 -> 'a', 2 -> 'b'))
+generalSize(math.Pi)
+
+def isInstance(expr: Any): Int =
+  if (expr.isInstanceOf[String]) {
+    val s = expr.asInstanceOf[String]
+    s.length
+  } else 0
+
+"isInstance(\"foo\") [" + isInstance("foo") + "]"
+
+def isIntIntMap(x: Any): Boolean =
+  x match
+    case m: Map[Int, Int] => true
+    case _  => false
+
+isIntIntMap(Map(1 -> 1))
+isIntIntMap(Map("abc" -> "abc"))
+
+// Arrays are checked at runtime
+def isStringArray(x: Any): String =
+  x match
+    case a: Array[String] => "yes"
+    case _  => "no"
+
+isStringArray(Array("abc"))
+isStringArray(Array(1, 2, 3))
+
+// Variable binding
+
+def matchUnOp(expr: Expr) =
+  expr match
+    case UnOp("abs", e @ UnOp("abs", _)) => e
+    case _  =>
+
+// 13.3 Pattern guards
+
+def simplifyAdd(e: Expr): Expr =
+  e match
+    case BinOp("+", x, y) if x == y =>
+      BinOp("*", x, Num(2))
+    case _  => e
+
+simplifyAdd(BinOp("+", Num(4), Num(4)))
+
+// 13.4 Pattern overlaps
+// Listing 13.15 · Match expression in which case order matters.
+
+def simplifyAll(expr: Expr): Expr =
+  expr match
+    case UnOp("-", UnOp("-", e)) =>
+      simplifyAll(e)
+    case BinOp("+", e, Num(0)) =>
+      simplifyAll(e)
+    case BinOp("*", e, Num(1)) =>
+      simplifyAll(e)
+    case UnOp(op, e) =>
+      UnOp(op, simplifyAll(e))
+    case BinOp(op, l, r) =>
+      BinOp(op, simplifyAll(l), simplifyAll(r))
+    case _ => expr
+
