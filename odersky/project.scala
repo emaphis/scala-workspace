d70cb167aa8f6077c5431c2938cd3b5f0d4681e0
Ed <emaphis85@gmail.com>
2026-06-20 16:44:15 -0400

    Odersky: Large scale reorganization to conform to scala-cli project layoutl.

--- odersky/project.scala
+++ odersky/project.scala
@@ -1,3 +1,5 @@
 //> using scala 3.8.4        // or whatever version
-// using options -Werror   // example compiler options
 //> using options -source:3.8
+
+//> using options -feature
+//> using options -deprecation
